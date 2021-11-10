749b9d31e25ec4b8348ec9a9d5910b78e6946a40
computer <imac-computer@computers-iMac.local>
2021-11-10 14:05:10 -0500

    f

--- hasql.nixc.us/zsh-setup.sh
+++ hasql.nixc.us/zsh-setup.sh
@@ -5,7 +5,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # echo y|sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh)
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="pygmalion"/g' /root/.zshrc
-sed -i 's/bash /root/zsh-setup.sh//d' /root/.profile
+sed -i 's\bash /root/zsh-setup.sh\\' /root/.profile
 rm -f /root/zsh-setup.sh
 echo "Relog into terminal finished bootstrapping server"
 exit
