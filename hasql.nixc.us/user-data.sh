a9ab11dcb647c38c211565e9ce62d6cb25570003
computer <imac-computer@computers-iMac.local>
2021-11-10 12:39:51 -0500

    f

--- hasql.nixc.us/user-data.sh
+++ hasql.nixc.us/user-data.sh
@@ -26,8 +26,8 @@ apt-get install -y iftop htop glances zsh glusterfs-server glusterfs-client salt
 
 ## Setup ZSH and ohmyzsh theme
 cd /root/
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-echo y|sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+#echo y|sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s /bin/zsh
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="pygmalion"/g' /root/.zshrc
 
