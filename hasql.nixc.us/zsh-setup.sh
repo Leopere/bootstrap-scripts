62e14c434dbf71eeb006396bbf59d6f8a3769787
computer <imac-computer@computers-iMac.local>
2021-11-10 14:58:24 -0500

    f

--- hasql.nixc.us/zsh-setup.sh
+++ hasql.nixc.us/zsh-setup.sh
@@ -3,7 +3,6 @@
 # cd /root/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 # echo y|sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# chsh -s $(which zsh)
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="pygmalion"/' /root/.zshrc
 echo 'export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)' >> /root/.zshrc
 echo 'export PUBLIC_IPV6=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv6/address)' >> /root/.zshrc
@@ -18,10 +17,13 @@ echo 'export PUBLIC_IPV6=$(curl -s http://169.254.169.254/metadata/v1/interfaces
   git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
 ## Enable plugins
-sed -i 's\plugins=(git)\plugins=(git cp colored-man-pages docker docker-compose extract iterm2 python rsync safe-paste transfer ubuntu zsh_reload zsh-navigation-tools zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search)\' /root/.zshrc
+sed -i 's\plugins=(git)\plugins=(git cp colored-man-pages docker docker-compose extract iterm2 python rsync safe-paste transfer ubuntu zsh-navigation-tools zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search)\' /root/.zshrc
 
 
 ## Cleanup
 sed -i 's\bash /root/zsh-setup.sh\\' /root/.profile
 rm -f /root/zsh-setup.sh
 echo "Relog into terminal finished bootstrapping server"
+chsh -s $(which zsh)
+zsh
+source /root/.zshrc
