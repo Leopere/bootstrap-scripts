c6536d61218fbd128a0799ea4c9a3286d59e8e48
computer <imac-computer@computers-iMac.local>
2021-12-21 11:42:46 -0500

    f

--- hasql.nixc.us/user-data.sh
+++ hasql.nixc.us/user-data.sh
@@ -9,12 +9,15 @@ set -e
 ## source <(curl -s https://digitalocean-user-data:sHEG3NTC6og8pCJDTF6EPYb8jLmbskx5Ns@git.nixc.us/Colin_/do-userdata/raw/branch/master/hasql.nixc.us/user-data.sh)
 
 curl -sL https://sentry.io/get-cli/ | bash
-# export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
-# echo $HOSTNAME > /etc/hostname
-# hostname -F /etc/hostname
-# hostname -f
-# export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
-# export PUBLIC_IPV6=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv6/address)
+
+## BLOCK THIS OUT IF NOT DEPLOYING TO DIGITALOCEAN ##
+export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
+echo $HOSTNAME > /etc/hostname
+hostname -F /etc/hostname
+hostname -f
+export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+export PUBLIC_IPV6=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv6/address)
+## BLOCK THIS OUT IF NOT DEPLOYING TO DIGITALOCEAN ##
 
 ## Installing Salt for Ubuntu 20.04
 curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest/salt-archive-keyring.gpg
