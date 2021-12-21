9b3597e296e869e441c3d19a8e770b2958d862a7
computer <imac-computer@computers-iMac.local>
2021-12-21 11:55:36 -0500

    f

--- hasql.nixc.us/user-data.sh
+++ hasql.nixc.us/user-data.sh
@@ -11,12 +11,12 @@ set -e
 curl -sL https://sentry.io/get-cli/ | bash
 
 ## BLOCK THIS OUT IF NOT DEPLOYING TO DIGITALOCEAN ##
-export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
-echo $HOSTNAME > /etc/hostname
-hostname -F /etc/hostname
-hostname -f
-export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
-export PUBLIC_IPV6=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv6/address)
+# export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
+# echo $HOSTNAME > /etc/hostname
+# hostname -F /etc/hostname
+# hostname -f
+# export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
+# export PUBLIC_IPV6=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv6/address)
 ## BLOCK THIS OUT IF NOT DEPLOYING TO DIGITALOCEAN ##
 
 ## Installing Salt for Ubuntu 20.04
